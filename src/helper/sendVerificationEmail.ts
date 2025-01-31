e786d870fd35f8e295ca06b1e48ca159a14e9a03
Arnab Mukherjee <136512754+arnab-git-404@users.noreply.github.com>
2025-01-31 16:14:12 +0530

    Update sendVerificationEmail.ts

--- src/helper/sendVerificationEmail.ts
+++ src/helper/sendVerificationEmail.ts
@@ -2,7 +2,7 @@ import { ApiResponse } from "@/customTypes/ApiResponse";
 import { google } from "googleapis";
 import nodemailer from "nodemailer";
 
-const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
 const REDIRECT_URI = process.env.REDIRECT_URI
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN
